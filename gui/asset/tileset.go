dcc1f772772cb5d918322e05b1b66770f0d9efab
kim <kybinz@gmail.com>
2024-12-20 00:45:50 +0900

    add reference for asset

--- gui/asset/tileset.go
+++ gui/asset/tileset.go
@@ -3,25 +3,28 @@ package asset
 import "image"
 
 type TileSet struct {
-	Author   string
-	License  string
-	File     string
-	TileSize int
-	BaseImg  image.Image
+	Reference string
+	Author    string
+	License   string
+	File      string
+	TileSize  int
+	BaseImg   image.Image
 }
 
 var basictiles = &TileSet{
-	Author:   "Lanea Zimmerman",
-	License:  "anti-DRM clause of CC-BY 3.0",
-	File:     "asset/basictiles.png",
-	TileSize: 16,
+	Reference: "https://opengameart.org/content/tiny-16-basic",
+	Author:    "Lanea Zimmerman",
+	License:   "anti-DRM clause of CC-BY 3.0",
+	File:      "asset/basictiles.png",
+	TileSize:  16,
 }
 
 var overworld = &TileSet{
-	Author:   "ArMM1998",
-	License:  "CC0",
-	File:     "asset/overworld.png",
-	TileSize: 16,
+	Reference: "https://opengameart.org/content/zelda-like-tilesets-and-sprites",
+	Author:    "ArMM1998",
+	License:   "CC0",
+	File:      "asset/overworld.png",
+	TileSize:  16,
 }
 
 var TileSets = map[string]*TileSet{
