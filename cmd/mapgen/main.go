77558ef3c807b6e7e60fec297a611a3ce5af5625
kim <kybinz@gmail.com>
2025-01-22 16:22:31 +0900

    fetch and modify gui to use shiny nodes system

--- cmd/mapgen/main.go
+++ cmd/mapgen/main.go
@@ -1,257 +1,85 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"log"
 
 	"golang.org/x/exp/shiny/driver"
 	"golang.org/x/exp/shiny/screen"
+	"golang.org/x/exp/shiny/unit"
+	"golang.org/x/exp/shiny/widget"
+	"golang.org/x/exp/shiny/widget/node"
 	"golang.org/x/image/draw"
-	"golang.org/x/mobile/event/key"
-	"golang.org/x/mobile/event/lifecycle"
-	"golang.org/x/mobile/event/mouse"
-	"golang.org/x/mobile/event/paint"
-	"golang.org/x/mobile/event/size"
 )
 
-type Widget interface {
-	SetRect(image.Rectangle)
-	HandleEvent(any)
-	Draw(screen.Window, screen.Screen)
+func stretch(n node.Node, alongWeight int) node.Node {
+	return widget.WithLayoutData(n, widget.FlowLayoutData{
+		AlongWeight:  alongWeight,
+		ExpandAlong:  true,
+		ShrinkAlong:  true,
+		ExpandAcross: true,
+		ShrinkAcross: true,
+	})
 }
 
-type Area struct {
-	Name   string
-	Rect   image.Rectangle
-	Split  *AreaSplit
-	Widget Widget
+// Uniform is a shell widget that paints a uniform color, analogous to an
+// image.Uniform.
+type Uniform struct {
+	node.ShellEmbed
+	Color color.Color
 }
 
-func resize(a *Area, rect image.Rectangle) {
-	a.Rect = rect
-	if a.Widget != nil {
-		a.Widget.SetRect(rect)
+// NewUniform returns a new Uniform widget of the given color.
+func NewUniform(c color.Color, inner node.Node) *Uniform {
+	w := &Uniform{
+		Color: c,
 	}
-	if a.Split == nil {
-		return
+	w.Wrapper = w
+	if inner != nil {
+		w.Insert(inner, nil)
 	}
-	s := a.Split
-	switch a.Split.Dir {
-	case SplitLeft:
-		x := min(rect.Min.X+s.Dist, rect.Max.X)
-		resize(s.A, image.Rect(rect.Min.X, rect.Min.Y, x, rect.Max.Y))
-		resize(s.B, image.Rect(x, rect.Min.Y, rect.Max.X, rect.Max.Y))
-	case SplitRight:
-		x := max(rect.Min.X, rect.Max.X-s.Dist)
-		resize(s.A, image.Rect(rect.Min.X, rect.Min.Y, x, rect.Max.Y))
-		resize(s.B, image.Rect(x, rect.Min.Y, rect.Max.X, rect.Max.Y))
-	case SplitTop:
-		y := min(rect.Min.Y+s.Dist, rect.Max.Y)
-		resize(s.A, image.Rect(rect.Min.X, rect.Min.Y, rect.Max.X, y))
-		resize(s.B, image.Rect(rect.Min.X, y, rect.Max.X, rect.Max.Y))
-	case SplitBottom:
-		y := max(rect.Min.Y, rect.Max.Y-s.Dist)
-		resize(s.A, image.Rect(rect.Min.X, rect.Min.Y, rect.Max.X, y))
-		resize(s.B, image.Rect(rect.Min.X, y, rect.Max.X, rect.Max.Y))
-	}
+	return w
 }
 
-func named(a *Area, m map[string]*Area) {
-	if a.Name != "" {
-		m[a.Name] = a
+func (w *Uniform) PaintBase(ctx *node.PaintBaseContext, origin image.Point) error {
+	w.Marks.UnmarkNeedsPaintBase()
+	r := w.Rect.Add(origin)
+	img := image.NewUniform(w.Color)
+	draw.Draw(ctx.Dst, r, img, image.Point{}, draw.Src)
+	if c := w.FirstChild; c != nil {
+		return c.Wrapper.PaintBase(ctx, origin.Add(w.Rect.Min))
 	}
-	if a.Split == nil {
-		return
-	}
-	if a.Split.A != nil {
-		named(a.Split.A, m)
-	}
-	if a.Split.B != nil {
-		named(a.Split.B, m)
-	}
-}
-
-func inside(r image.Rectangle, pt image.Point) bool {
-	if pt.X < r.Min.X || pt.X > r.Max.X {
-		return false
-	}
-	if pt.Y < r.Min.Y || pt.Y > r.Max.Y {
-		return false
-	}
-	return true
-}
-
-func WidgetAt(a *Area, pt image.Point) Widget {
-	if a == nil {
-		return nil
-	}
-	if !inside(a.Rect, pt) {
-		return nil
-	}
-	// area rect contains pt
-	if a.Split == nil {
-		return a.Widget
-	}
-	if w := WidgetAt(a.Split.A, pt); w != nil {
-		return w
-	}
-	if w := WidgetAt(a.Split.B, pt); w != nil {
-		return w
-	}
-	return a.Widget
-}
-
-type AreaSplit struct {
-	Dir  SplitDir
-	Dist int
-	A    *Area // Top or Left according to SplitDir
-	B    *Area // Bottom or Right according to SplitDir
-}
-
-type SplitDir int
-
-const (
-	SplitLeft = SplitDir(iota)
-	SplitRight
-	SplitTop
-	SplitBottom
-)
-
-func drawColor(w screen.Window, s screen.Screen, rect image.Rectangle, c color.Color) {
-	size := rect.Max.Sub(rect.Min)
-	tex, err := s.NewTexture(size)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	buf, err := s.NewBuffer(size)
-	if err != nil {
-		tex.Release()
-		log.Println(err)
-		return
-	}
-	img := image.NewUniform(c)
-	draw.Copy(buf.RGBA(), image.Point{}, img, buf.Bounds(), draw.Src, nil)
-	tex.Upload(image.Point{}, buf, tex.Bounds())
-	buf.Release()
-	w.Copy(rect.Min, tex, tex.Bounds(), screen.Src, nil)
-}
-
-type board struct {
-	rect  image.Rectangle
-	dirty bool
-	size  image.Point
-	bg    color.Color
-}
-
-func (b *board) SetRect(r image.Rectangle) {
-	if b.rect == r {
-		return
-	}
-	b.rect = r
-	b.dirty = true
-}
-
-func (b *board) HandleEvent(e any) {
-	fmt.Println(e)
-}
-
-func (b *board) Draw(w screen.Window, s screen.Screen) {
-	if !b.dirty {
-		return
-	}
-	drawColor(w, s, b.rect, b.bg)
-	b.dirty = false
-}
-
-type position struct {
-	X float32
-	Y float32
-}
-
-func pos(x, y float32) position {
-	return position{X: x, Y: y}
+	return nil
 }
 
 func main() {
-	area := &Area{
-		Name: "bg",
-		Split: &AreaSplit{
-			Dir:  SplitLeft,
-			Dist: 200, // px
-			A:    &Area{Name: "side"},
-			B:    &Area{Name: "playground"},
-		},
-	}
-	namedArea := make(map[string]*Area)
-	named(area, namedArea)
-
-	playground := &board{dirty: true, bg: color.NRGBA{255, 255, 0, 255}}
-	namedArea["playground"].Widget = playground
-
-	side := &board{dirty: true, bg: color.NRGBA{0, 255, 255, 255}}
-	namedArea["side"].Widget = side
-
-	widgets := make([]Widget, 0)
-	for _, a := range namedArea {
-		if a.Widget != nil {
-			widgets = append(widgets, a.Widget)
-		}
-	}
-	winSize := image.Pt(400, 300)
-	resize(area, image.Rect(0, 0, winSize.X, winSize.Y))
-	mousePos := image.Pt(-1, -1)
 	driver.Main(func(s screen.Screen) {
-		w, err := s.NewWindow(&screen.NewWindowOptions{
-			Title:  "Board GUI",
-			Width:  winSize.X,
-			Height: winSize.Y,
-		})
+		side := widget.NewSizer(unit.DIPs(400), unit.Value{},
+			NewUniform(color.RGBA{255, 128, 128, 255},
+				widget.NewPadder(widget.AxisBoth, unit.Ems(0.5),
+					widget.NewLabel("hi"),
+				),
+			),
+		)
+		stg := NewStage()
+		err := stg.Setup()
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer w.Release()
-		paintPending := true
-		w.Send(paint.Event{})
-		for {
-			switch e := w.NextEvent().(type) {
-			case lifecycle.Event:
-				if e.To == lifecycle.StageDead {
-					return
-				}
-			case key.Event:
-				if e.Code == key.CodeEscape {
-					return
-				}
-				wg := WidgetAt(area, mousePos)
-				if wg != nil {
-					wg.HandleEvent(e)
-				}
-				if !paintPending {
-					paintPending = true
-					w.Send(paint.Event{})
-				}
-			case paint.Event:
-				resize(area, image.Rect(0, 0, winSize.X, winSize.Y))
-				for _, wg := range widgets {
-					wg.Draw(w, s)
-				}
-				w.Publish()
-				paintPending = false
-			case mouse.Event:
-				mousePos = image.Pt(int(e.X), int(e.Y))
-			case size.Event:
-				winSize = image.Pt(e.WidthPx, e.HeightPx)
-				resize(area, image.Rect(0, 0, winSize.X, winSize.Y))
-				if !paintPending {
-					paintPending = true
-					w.Send(paint.Event{})
-				}
-			case error:
-				log.Print(e)
-			}
+		body := NewUniform(color.RGBA{255, 255, 255, 255},
+			stg,
+		)
+		main := widget.NewFlow(widget.AxisHorizontal,
+			stretch(widget.NewSheet(side), 0),
+			stretch(widget.NewSheet(body), 1),
+		)
+		if err := widget.RunWindow(s, main, &widget.RunWindowOptions{
+			NewWindowOptions: screen.NewWindowOptions{
+				Title: "Hello",
+			},
+		}); err != nil {
+			log.Fatal(err)
 		}
 	})
 }
---
+++ cmd/mapgen/stage.go
@@ -0,0 +1,306 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	_ "image/png"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/BurntSushi/toml"
+	"golang.org/x/exp/shiny/widget/node"
+	"golang.org/x/image/draw"
+	"golang.org/x/mobile/event/key"
+	"golang.org/x/mobile/event/mouse"
+)
+
+type ImageLayer []SrcPos
+
+type SrcPos struct {
+	T, I, J int
+}
+
+type TileSet struct {
+	Reference string
+	Link      string
+	Unzip     string
+	Author    string
+	License   string
+	File      string
+	TileSize  int
+}
+
+func loadImage(name string) (image.Image, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
+
+type Stage struct {
+	node.LeafEmbed
+	TileCount   image.Point
+	TileSets    []*TileSet
+	TileSize    image.Point
+	Layers      []ImageLayer // from bg to fg
+	ImgAt       map[SrcPos]image.Image
+	CursorImg   image.Image
+	HoverImg    image.Image
+	CursorPos   [2]int
+	HoverPos    [2]int
+	CursorColor color.NRGBA
+	HoverColor  color.NRGBA
+	DragFrom    image.Point
+	Dragging    bool
+	Offset      image.Point
+}
+
+func NewStage() *Stage {
+	f, err := os.Open("asset/tileset.toml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	d := toml.NewDecoder(f)
+	tilesets := make(map[string]*TileSet)
+	_, err = d.Decode(&tilesets)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := &Stage{
+		TileCount: image.Pt(3, 4),
+		TileSize:  image.Pt(32, 32),
+		TileSets: []*TileSet{
+			tilesets["basictiles"],
+			tilesets["overworld"],
+		},
+		Layers: []ImageLayer{
+			ImageLayer{
+				{T: 0, I: 0, J: 0},
+				{T: 0, I: 1, J: 0},
+				{T: 0, I: 2, J: 0},
+				{T: 0, I: 0, J: 1},
+				{T: 0, I: 1, J: 1},
+				{T: 0, I: 2, J: 1},
+				{T: 0, I: 0, J: 2},
+				{T: 0, I: 1, J: 2},
+				{T: 0, I: 2, J: 2},
+				{T: 1, I: 0, J: 0},
+				{T: 1, I: 0, J: 1},
+				{T: 1, I: 0, J: 2},
+			},
+			ImageLayer{
+				{T: 1, I: 0, J: 0},
+				{T: 1, I: 1, J: 0},
+				{T: 1, I: 2, J: 0},
+				{T: 1, I: 0, J: 1},
+				{T: 1, I: 1, J: 1},
+				{T: 1, I: 2, J: 1},
+				{T: 1, I: 0, J: 2},
+				{T: 1, I: 1, J: 2},
+				{T: 1, I: 2, J: 2},
+				{T: 0, I: 0, J: 0},
+				{T: 0, I: 0, J: 1},
+				{T: 0, I: 0, J: 2},
+			},
+		},
+		HoverPos:    [2]int{-1, -1},
+		CursorColor: color.NRGBA{64, 192, 0, 255},
+		HoverColor:  color.NRGBA{192, 192, 192, 255},
+	}
+	s.Wrapper = s
+	return s
+}
+
+func (s *Stage) Setup() error {
+	if s.TileCount.X < 0 || s.TileCount.Y < 0 {
+		return fmt.Errorf("stage size couldn't be negative: got %v", s.TileCount)
+	}
+	if s.TileSize.X <= 0 || s.TileSize.Y <= 0 {
+		return fmt.Errorf("tile size needs 1 or more pixels : got %v", s.TileSize)
+	}
+	for i, layer := range s.Layers {
+		if s.TileCount.X*s.TileCount.Y != len(layer) {
+			return fmt.Errorf("tex layer %d size different from stage size", i)
+		}
+	}
+	baseImgs := make([]image.Image, len(s.TileSets))
+	for i, atx := range s.TileSets {
+		img, err := loadImage(filepath.Join("asset", atx.File))
+		if err != nil {
+			log.Println(err)
+			img = image.White
+		}
+		baseImgs[i] = img
+	}
+	posMap := make(map[SrcPos]bool)
+	for _, layer := range s.Layers {
+		for _, pos := range layer {
+			posMap[pos] = true
+		}
+	}
+	s.ImgAt = make(map[SrcPos]image.Image)
+	for pos := range posMap {
+		t, i, j := pos.T, pos.I, pos.J
+		if t >= len(s.TileSets) {
+			return fmt.Errorf("invalid tileset index: %v, only %v exists", t, len(s.TileSets))
+		}
+		ts := s.TileSets[t]
+		sz := ts.TileSize
+		src := baseImgs[t]
+		srcBound := image.Rect(i*sz, j*sz, (i+1)*sz, (j+1)*sz)
+		tileImg := image.NewNRGBA(image.Rect(0, 0, s.TileSize.X, s.TileSize.Y))
+		draw.NearestNeighbor.Scale(tileImg, tileImg.Rect, src, srcBound, draw.Src, nil)
+		s.ImgAt[pos] = tileImg
+	}
+	cursorPoints := make([]image.Point, 0)
+	for y := 0; y < s.TileSize.Y; y++ {
+		for x := 0; x < s.TileSize.X; x++ {
+			if x == 0 || y == 0 || x == s.TileSize.X-1 || y == s.TileSize.Y-1 {
+				cursorPoints = append(cursorPoints, image.Pt(x, y))
+			}
+		}
+	}
+	cs := image.NewNRGBA(image.Rect(0, 0, s.TileSize.X, s.TileSize.Y))
+	for _, p := range cursorPoints {
+		cs.SetNRGBA(p.X, p.Y, s.CursorColor)
+	}
+	s.CursorImg = cs
+	hv := image.NewNRGBA(image.Rect(0, 0, s.TileSize.X, s.TileSize.Y))
+	for _, p := range cursorPoints {
+		hv.SetNRGBA(p.X, p.Y, s.HoverColor)
+	}
+	s.HoverImg = hv
+	return nil
+}
+
+func (s *Stage) TileImgs(p image.Point) []image.Image {
+	idx := p.Y*s.TileCount.X + p.X
+	imgs := make([]image.Image, 0, len(s.Layers))
+	for _, layer := range s.Layers {
+		ip := layer[idx]
+		imgs = append(imgs, s.ImgAt[ip])
+	}
+	return imgs
+}
+
+func (s *Stage) PaintBase(ctx *node.PaintBaseContext, origin image.Point) error {
+	s.Marks.UnmarkNeedsPaintBase()
+	r := s.Rect.Add(origin)
+	topLeft := image.Pt(s.Offset.X-s.Rect.Max.X/2+s.TileSize.X/2*s.TileCount.X, s.Offset.Y-s.Rect.Max.Y/2+s.TileSize.Y/2*s.TileCount.Y)
+	var wg sync.WaitGroup
+	for y := -(topLeft.Y & 0x7f); y < s.Rect.Max.Y; y += s.TileSize.Y {
+		for x := -(topLeft.X & 0x7f); x < s.Rect.Max.X; x += s.TileSize.X {
+			wg.Add(1)
+			go func(x, y int) {
+				defer wg.Done()
+				ip := image.Point{
+					(x + topLeft.X) / s.TileSize.X,
+					(y + topLeft.Y) / s.TileSize.Y,
+				}
+				if ip.X < 0 || ip.X >= s.TileCount.X || ip.Y < 0 || ip.Y >= s.TileCount.Y {
+					return
+				}
+				dr := r.Add(image.Point{x, y})
+				imgs := s.TileImgs(ip)
+				for _, m := range imgs {
+					draw.Draw(ctx.Dst, dr, m, image.Point{}, draw.Over)
+				}
+			}(x, y)
+		}
+	}
+	wg.Wait()
+	if s.HoverPos[0] != -1 {
+		dr := r.Add(image.Point{-topLeft.X + s.HoverPos[0]*s.TileSize.X, -topLeft.Y + s.HoverPos[1]*s.TileSize.Y})
+		draw.Draw(ctx.Dst, dr, s.HoverImg, image.Point{}, draw.Over)
+	}
+	dr := r.Add(image.Point{-topLeft.X + s.CursorPos[0]*s.TileSize.X, -topLeft.Y + s.CursorPos[1]*s.TileSize.Y})
+	draw.Draw(ctx.Dst, dr, s.CursorImg, image.Point{}, draw.Over)
+	return nil
+}
+
+func (s *Stage) OnInputEvent(e any, origin image.Point) node.EventHandled {
+	switch e := e.(type) {
+	case key.Event:
+		if e.Direction == key.DirPress {
+			if e.Code == key.CodeLeftArrow {
+				s.CursorPos[0]--
+				if s.CursorPos[0] < 0 {
+					s.CursorPos[0] = 0
+				}
+				s.Mark(node.MarkNeedsPaintBase)
+				break
+			}
+			if e.Code == key.CodeRightArrow {
+				s.CursorPos[0]++
+				if s.CursorPos[0] >= s.TileCount.X {
+					s.CursorPos[0] = s.TileCount.X - 1
+				}
+				s.Mark(node.MarkNeedsPaintBase)
+				break
+			}
+			if e.Code == key.CodeUpArrow {
+				s.CursorPos[1]--
+				if s.CursorPos[1] < 0 {
+					s.CursorPos[1] = 0
+				}
+				s.Mark(node.MarkNeedsPaintBase)
+				break
+			}
+			if e.Code == key.CodeDownArrow {
+				s.CursorPos[1]++
+				if s.CursorPos[1] >= s.TileCount.Y {
+					s.CursorPos[1] = s.TileCount.Y - 1
+				}
+				s.Mark(node.MarkNeedsPaintBase)
+				break
+			}
+		}
+	case mouse.Event:
+		p := image.Point{X: int(e.X), Y: int(e.Y)}.Sub(origin)
+		topLeft := image.Pt(s.Offset.X-s.Rect.Max.X/2+s.TileSize.X/2*s.TileCount.X, s.Offset.Y-s.Rect.Max.Y/2+s.TileSize.Y/2*s.TileCount.Y)
+		x := (p.X + topLeft.X) / s.TileSize.X
+		y := (p.Y + topLeft.Y) / s.TileSize.Y
+		hx := x
+		hy := y
+		if hx < 0 || hx >= s.TileCount.X || hy < 0 || hy >= s.TileCount.Y {
+			hx = -1
+			hy = -1
+		}
+		if hx != s.HoverPos[0] || hy != s.HoverPos[1] {
+			s.HoverPos[0] = hx
+			s.HoverPos[1] = hy
+			s.Mark(node.MarkNeedsPaintBase)
+		}
+		if e.Button == mouse.ButtonLeft && e.Direction == mouse.DirRelease {
+			if x < 0 || x >= s.TileCount.X || y < 0 || y >= s.TileCount.Y {
+				break
+			}
+			if x != s.CursorPos[0] || y != s.CursorPos[1] {
+				s.CursorPos[0] = x
+				s.CursorPos[1] = y
+				s.Mark(node.MarkNeedsPaintBase)
+				break
+			}
+		}
+		if e.Button == mouse.ButtonMiddle && e.Direction != mouse.DirNone {
+			s.Dragging = e.Direction == mouse.DirPress
+			s.DragFrom = p
+			break
+		}
+		if !s.Dragging {
+			break
+		}
+		s.Offset = s.Offset.Sub(p.Sub(s.DragFrom))
+		s.DragFrom = p
+		s.Mark(node.MarkNeedsPaintBase)
+	}
+	return node.Handled
+}
