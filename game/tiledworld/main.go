37672e5d79a26b853fa16da3141be5182d6ea28d
kim <kybinz@gmail.com>
2026-02-16 00:09:16 +0900

    game/tiledworld: implement basic pixel drawing

--- game/tiledworld/main.go
+++ game/tiledworld/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"log"
@@ -13,6 +14,7 @@ const (
 	tileSize     = 16
 	layoutWidth  = 320
 	layoutHeight = 240
+	zoomScale    = 8
 )
 
 type World struct {
@@ -31,7 +33,7 @@ func NewWorld() *World {
 type Map map[image.Point]*Tile
 
 type Tile struct {
-	Img image.RGBA
+	Image *image.RGBA
 }
 
 type Character struct {
@@ -39,6 +41,8 @@ type Character struct {
 	Level      int
 	Pos        image.Point
 	CopiedTile *Tile
+	InZoomMode bool
+	ZoomPos    image.Point // cannot exceed (tileSize, tileSize)
 }
 
 func (ch *Character) Move(pt image.Point) {
@@ -49,7 +53,7 @@ func (ch *Character) SetLevel(l int) {
 	ch.Level = l
 }
 
-func (ch *Character) StandingTile() *Tile {
+func (ch *Character) CurrentTile() *Tile {
 	return ch.World.Map[ch.Pos]
 }
 
@@ -76,21 +80,130 @@ func (g *Game) Update() error {
 	if inpututil.IsKeyJustPressed(ebiten.KeyQ) {
 		return ebiten.Termination
 	}
-	p := g.Char.Pos
-	if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) {
-		p = p.Add(image.Pt(-1, 0))
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		g.Char.InZoomMode = !g.Char.InZoomMode
 	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) {
-		p = p.Add(image.Pt(1, 0))
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) {
-		p = p.Add(image.Pt(0, -1))
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) {
-		p = p.Add(image.Pt(0, 1))
-	}
-	if p.In(g.World.Bound) {
-		g.Char.Pos = p
+	if g.Char.InZoomMode {
+		if inpututil.IsKeyJustPressed(ebiten.KeyR) {
+			tile := g.Char.CurrentTile()
+			if tile == nil {
+				tile = &Tile{}
+				tile.Image = image.NewRGBA(image.Rect(0, 0, tileSize, tileSize))
+				g.World.Map[g.Char.Pos] = tile
+			}
+			zp := g.Char.ZoomPos
+			tile.Image.Set(zp.X, zp.Y, color.RGBA{R: 255, A: 255})
+			return nil
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyG) {
+			tile := g.Char.CurrentTile()
+			if tile == nil {
+				tile = &Tile{}
+				tile.Image = image.NewRGBA(image.Rect(0, 0, tileSize, tileSize))
+				g.World.Map[g.Char.Pos] = tile
+			}
+			zp := g.Char.ZoomPos
+			tile.Image.Set(zp.X, zp.Y, color.RGBA{G: 255, A: 255})
+			return nil
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyB) {
+			tile := g.Char.CurrentTile()
+			if tile == nil {
+				tile = &Tile{}
+				tile.Image = image.NewRGBA(image.Rect(0, 0, tileSize, tileSize))
+				g.World.Map[g.Char.Pos] = tile
+			}
+			zp := g.Char.ZoomPos
+			tile.Image.Set(zp.X, zp.Y, color.RGBA{B: 255, A: 255})
+			return nil
+		}
+		zp := g.Char.ZoomPos
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) {
+			zp = zp.Add(image.Pt(-1, 0))
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) {
+			zp = zp.Add(image.Pt(1, 0))
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) {
+			zp = zp.Add(image.Pt(0, -1))
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) {
+			zp = zp.Add(image.Pt(0, 1))
+		}
+		if zp.In(image.Rect(0, 0, tileSize, tileSize)) {
+			g.Char.ZoomPos = zp
+			return nil
+		}
+		// user go outside of the tile
+		p := g.Char.Pos
+		if zp.X < 0 {
+			p = p.Add(image.Pt(-1, 0))
+		}
+		if zp.X >= tileSize {
+			p = p.Add(image.Pt(1, 0))
+		}
+		if zp.Y < 0 {
+			p = p.Add(image.Pt(0, -1))
+		}
+		if zp.Y >= 0 {
+			p = p.Add(image.Pt(0, 1))
+		}
+		if !p.In(g.World.Bound) {
+			if zp.X < 0 {
+				zp.X = 0
+			}
+			if zp.X >= tileSize {
+				zp.X = tileSize - 1
+			}
+			if zp.Y < 0 {
+				zp.Y = 0
+			}
+			if zp.Y >= tileSize {
+				zp.Y = tileSize - 1
+			}
+		} else {
+			// moved to a new tile
+			g.Char.Pos = p
+			if zp.X < 0 {
+				zp.X = tileSize - 1
+			}
+			if zp.X >= tileSize {
+				zp.X = 0
+			}
+			if zp.Y < 0 {
+				zp.Y = tileSize - 1
+			}
+			if zp.Y >= tileSize {
+				zp.Y = 0
+			}
+		}
+	} else {
+		if inpututil.IsKeyJustPressed(ebiten.KeyC) {
+			g.Char.CopiedTile = g.Char.CurrentTile()
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyV) {
+			if g.Char.CopiedTile == nil {
+				delete(g.World.Map, g.Char.Pos)
+			} else {
+				g.World.Map[g.Char.Pos] = g.Char.CopiedTile
+			}
+		}
+		p := g.Char.Pos
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) {
+			p = p.Add(image.Pt(-1, 0))
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) {
+			p = p.Add(image.Pt(1, 0))
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) {
+			p = p.Add(image.Pt(0, -1))
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) {
+			p = p.Add(image.Pt(0, 1))
+		}
+		if p.In(g.World.Bound) {
+			g.Char.Pos = p
+		}
 	}
 	return nil
 }
@@ -100,39 +213,85 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		return
 	}
 	g.introShown = true
-	camRect := image.Rect(0, 0, layoutWidth, layoutHeight)
-	tile := ebiten.NewImage(tileSize, tileSize)
-	c := color.RGBA{R: 255, G: 128, B: 128, A: 255}
-	for i := 0; i < tileSize; i++ {
-		tile.Set(i, 0, c)
-		tile.Set(i, tileSize-1, c)
-	}
-	for j := 0; j < tileSize; j++ {
-		tile.Set(0, j, c)
-		tile.Set(tileSize-1, j, c)
-	}
-	minPos := image.Pt(camRect.Min.X/tileSize, camRect.Min.Y/tileSize)
-	maxPos := image.Pt(camRect.Max.X/tileSize, camRect.Max.Y/tileSize)
-	for j := minPos.Y; j <= maxPos.Y; j++ {
-		for i := minPos.X; i <= maxPos.X; i++ {
-			op := &ebiten.DrawImageOptions{}
-			op.GeoM.Translate(float64(i)*tileSize, float64(j)*tileSize)
-			screen.DrawImage(tile, op)
+	if g.Char.InZoomMode {
+		screen.Clear()
+		// draw zoomed tile
+		zoomedTileSize := zoomScale * tileSize
+		center := image.Pt(layoutWidth/2+1, layoutHeight/2+1)
+		origin := image.Pt(center.X-zoomedTileSize/2, center.Y-zoomedTileSize/2)
+		tileImage := ebiten.NewImage(tileSize, tileSize)
+		tile := g.Char.CurrentTile()
+		if tile != nil {
+			fmt.Println(tile.Image.Pix)
+			tileImage.WritePixels(tile.Image.Pix)
 		}
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(zoomScale, zoomScale)
+		op.GeoM.Translate(float64(origin.X), float64(origin.Y))
+		screen.DrawImage(tileImage, op)
+		// draw cursor
+		cursorImage := ebiten.NewImage(zoomScale, zoomScale)
+		c := color.RGBA{R: 255, G: 255, B: 64, A: 128}
+		for i := 0; i < zoomScale; i++ {
+			cursorImage.Set(i, 0, c)
+			cursorImage.Set(i, zoomScale-1, c)
+		}
+		for j := 0; j < zoomScale; j++ {
+			cursorImage.Set(0, j, c)
+			cursorImage.Set(zoomScale-1, j, c)
+		}
+		op = &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(float64(origin.X)+float64(g.Char.ZoomPos.X)*zoomScale, float64(origin.Y)+float64(g.Char.ZoomPos.Y)*zoomScale)
+		screen.DrawImage(cursorImage, op)
+		// draw outline
+		outlineImage := ebiten.NewImage(1, 1)
+		c = color.RGBA{R: 255, G: 255, B: 255, A: 255}
+		outlineImage.Set(0, 0, c)
+		op = &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(1, float64(zoomedTileSize)+2)
+		op.GeoM.Translate(float64(origin.X)-1, float64(origin.Y)-1)
+		screen.DrawImage(outlineImage, op)
+		op.GeoM.Translate(float64(zoomedTileSize)+1, 0)
+		screen.DrawImage(outlineImage, op)
+		op = &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(float64(zoomedTileSize)+2, 1)
+		op.GeoM.Translate(float64(origin.X)-1, float64(origin.Y)-1)
+		screen.DrawImage(outlineImage, op)
+		op.GeoM.Translate(0, float64(zoomedTileSize)+1)
+		screen.DrawImage(outlineImage, op)
+	} else {
+		screen.Clear()
+		camRect := image.Rect(0, 0, layoutWidth, layoutHeight)
+		tileImage := ebiten.NewImage(tileSize, tileSize)
+		minPos := image.Pt(camRect.Min.X/tileSize, camRect.Min.Y/tileSize)
+		maxPos := image.Pt(camRect.Max.X/tileSize, camRect.Max.Y/tileSize)
+		for j := minPos.Y; j <= maxPos.Y; j++ {
+			for i := minPos.X; i <= maxPos.X; i++ {
+				tile := g.World.Map[image.Pt(i, j)]
+				if tile != nil {
+					tileImage.WritePixels(tile.Image.Pix)
+				} else {
+					tileImage.Clear()
+				}
+				op := &ebiten.DrawImageOptions{}
+				op.GeoM.Translate(float64(i)*tileSize, float64(j)*tileSize)
+				screen.DrawImage(tileImage, op)
+			}
+		}
+		cursorImage := ebiten.NewImage(tileSize, tileSize)
+		c := color.RGBA{R: 255, G: 255, B: 64, A: 255}
+		for i := 0; i < tileSize; i++ {
+			cursorImage.Set(i, 0, c)
+			cursorImage.Set(i, tileSize-1, c)
+		}
+		for j := 0; j < tileSize; j++ {
+			cursorImage.Set(0, j, c)
+			cursorImage.Set(tileSize-1, j, c)
+		}
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(float64(g.Char.Pos.X)*tileSize, float64(g.Char.Pos.Y)*tileSize)
+		screen.DrawImage(cursorImage, op)
 	}
-	tile = ebiten.NewImage(tileSize, tileSize)
-	c = color.RGBA{R: 255, G: 255, B: 64, A: 255}
-	for i := 0; i < tileSize; i++ {
-		tile.Set(i, 0, c)
-		tile.Set(i, tileSize-1, c)
-	}
-	for j := 0; j < tileSize; j++ {
-		tile.Set(0, j, c)
-		tile.Set(tileSize-1, j, c)
-	}
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(float64(g.Char.Pos.X)*tileSize, float64(g.Char.Pos.Y)*tileSize)
-	screen.DrawImage(tile, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
